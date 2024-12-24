68af4c29e31d33e7f93e35ab41dcc22590309fcf
Jack Masters <16mastejac@castle.bep.ac>
2024-12-24 11:50:57 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// Countdown section
+const { body } = document;
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secsEl = document.getElementById('secs');
+
+function calculateChristmasCountdown() {
+  const now = new Date();
+  const currentMonth = now.getMonth() + 1;
+  const currentDay = now.getDate();
+
+  // Figure out the year that the next Christmas will occur on
+  let nextChristmasYear = now.getFullYear();
+  if (currentMonth === 12 && currentDay > 25) {
+    nextChristmasYear += 1;
+  }
+
+  const nextChristmasDate = `Dec 25, ${nextChristmasYear} 0:0:0`;
+  const christmasDate = new Date(nextChristmasDate);
+  const timeLeft = christmasDate - now; // in milliseconds
+
+  let days = 0;
+  let hours = 0;
+  let mins = 0;
+  let secs = 0;
+
+  // Don't calculate the time left if it is Christmas day
+  if (currentMonth !== 12 || (currentMonth === 12 && currentDay !== 25)) {
+    days = Math.floor(timeLeft / 1000 / 60 / 60 / 24);
+    hours = Math.floor(timeLeft / 1000 / 60 / 60) % 24;
+    mins = Math.floor(timeLeft / 1000 / 60) % 60;
+    secs = Math.floor(timeLeft / 1000) % 60;
+  }
+  daysEl.innerHTML = days < 10 ? `0${days}` : days;
+  hoursEl.innerHTML = hours < 10 ? `0${hours}` : hours;
+  minsEl.innerHTML = mins < 10 ? `0${mins}` : mins;
+  secsEl.innerHTML = secs < 10 ? `0${secs}` : secs;
+}
+
+function createSnowFlake() {
+  const snow_flake = document.createElement('i');
+  snow_flake.classList.add('far');
+  snow_flake.classList.add('fa-snowflake');
+  snow_flake.style.left = `${Math.random() * window.innerWidth}px`;
+  snow_flake.style.animationDuration = `${Math.random() * 3 + 2}s`;
+  snow_flake.style.opacity = Math.random();
+
+  body.appendChild(snow_flake);
+
+  setTimeout(() => {
+    snow_flake.remove();
+  }, 5000);
+}
+
+setInterval(calculateChristmasCountdown, 1000);
+setInterval(createSnowFlake, 100);
